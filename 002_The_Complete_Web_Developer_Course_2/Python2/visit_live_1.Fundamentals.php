6865e63cb8a2c709dbfc448b828401e48d67e13f
Guillem Parrado <guillem404@gmail.com>
2017-10-21 19:02:43 +0200

    Afegit contingut

---
+++ 002_The_Complete_Web_Developer_Course_2/Python2/visit_live_1.Fundamentals.php
@@ -0,0 +1,1 @@
+<script> window.location = "http://localhost:3017/cgi-bin/1.Fundamentals.py"; </script>
---
+++ 002_The_Complete_Web_Developer_Course_2/Python2/visit_live_2.Mastermind_game.php
@@ -0,0 +1,1 @@
+<script> window.location = "http://localhost:3017/cgi-bin/2.Mastermind_game.py"; </script>
